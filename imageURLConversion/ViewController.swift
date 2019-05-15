2253fb66cad1c96ca75a320b9043d485d195d34f
Harish Hardy <mj.harishardy@gmail.com>
2019-05-15 10:36:22 +0530

    image added

--- imageURLConversion/ViewController.swift
+++ imageURLConversion/ViewController.swift
@@ -9,8 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    var stringX = "https://foodpalette.in//Chennai//images//thumbnails//RS-_Bakehouse_23_Ferrerro_Rocher_Cupcake.jpg"
+    
+    
+   
+    
+    
+    @IBOutlet weak var imageViewX: UIImageView!
+    
+    
+    
+    
 
+    
+    
+    
     override func viewDidLoad() {
+        
+        
+        let urlX = URL(string: stringX)
+       
+        let dataX = try?Data(contentsOf: urlX!)
+        
+        imageViewX.image = UIImage(data: dataX!)
+        
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
